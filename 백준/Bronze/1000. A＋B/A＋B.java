fff40965090200afa0fef9f58b87956d23d00034
shimssung <shimssung@gmail.com>
2025-01-09 14:53:08 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String args[]){
+		Scanner sca = new Scanner(System.in);
+		int A = sca.nextInt();
+        int B = sca.nextInt();
+		System.out.println(A + B);
+	}
+}
