a19eaef2100c0979f00b0a7ffc9e73e0aa1ba6e8
shimssung <shimssung@gmail.com>
2025-04-25 17:08:35 +0900

    [level 0] Title: 옹알이 (1), Time: 0.74 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -11,28 +11,14 @@ class Solution {
         String[] words = {"aya", "ye", "woo", "ma"};
 
         for (String babble : babbling) {
-            int idx = 0;              // 현재 문자열 위치 인덱스
-            boolean isValid = true;   // 발음 가능한지 여부
-
-            while (idx < babble.length()) {
-                boolean matched = false;
-
-                for (String word : words) {
-                    if (babble.startsWith(word, idx)) {
-
-                        idx += word.length();    // 인덱스를 발음 길이만큼 전진
-                        matched = true;
-                        break;
-                    }
-                }
-
-                if (!matched) {   // 네 개의 발음 중 하나도 매치 안 되면 실패
-                    isValid = false;
-                    break;
-                }
+            for(String word : words) {
+                babble = babble.replace(word, "!");
             }
-
-            if (isValid) count++;
+            babble = babble.replace("!", "");
+            if(babble.isEmpty()) {
+                count++;
+            }
+               
         }
 
         return count;
@@ -43,3 +29,4 @@ class Solution {
 // comment
 // 이전 방식은 replace 방식을 사용함 -> 문제발생 -> wyeoo 일때 ye가 먼저 삭제되고 남은 woo가 삭제되며 발음 가능한걸로 인식
 // startsWirth()를 사용해 문자열을 하나씩 쪼개서 처음시작하는 문자하나먼저 인식하도록 수정 w로 시작하면서 woo와 맞는지 확인
+// 더 좋은 방식 -> 기존 replace방식을 살려서 원래는 ""이렇게 바로 없애버렸지만 1과 같은 숫자를  넣으면 w1oo 이되어서 인식을 못하게 만들수있음.
