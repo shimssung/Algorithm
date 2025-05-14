183d831db74c0b7ea152117afe592eb63eb2aec9
shimssung <shimssung@gmail.com>
2025-05-14 19:18:13 +0900

    [level 1] Title: 명예의 전당 (1), Time: 6.51 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> topScore = new ArrayList<>();
+        
+        for(int i = 0; i<score.length; i++) {
+            topScore.add(score[i]); // 일단 추가
+            // 정렬(오름차순)
+            Collections.sort(topScore);
+            
+            if (topScore.size() > k) {
+                topScore.remove(topScore.get(0)); // k개 초과면 가장 낮은 점수 제거
+            }
+            
+            answer[i] = topScore.get(0); // 가장 낮은 점수 저장
+        }
+        
+        
+        return answer;
+    }
+}
