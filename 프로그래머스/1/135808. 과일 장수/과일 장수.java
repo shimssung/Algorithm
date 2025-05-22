6979e4664e7b975675c2b17521fe4e130130c312
shimssung <shimssung@gmail.com>
2025-05-22 22:18:25 +0900

    [level 1] Title: 과일 장수, Time: 83.02 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        // 점수 내림차순 정렬
+        Arrays.sort(score);
+
+        int length = score.length;
+
+        for (int i = 0; i + m <= length; i += m) {
+            int minInBox = score[length - i - m]; // 상자 내 최소 점수
+            answer += minInBox * m;
+        }
+        
+        
+        return answer;
+    }
+}
