71060928da5d8bb580d43d38f431f7ed504780c6
shimssung <shimssung@gmail.com>
2025-07-09 14:23:41 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.08 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,57 +1,49 @@
 class Solution {
     public int solution(int n, int w, int num) {
+        int answer = 0;
         
-        // 상자 개수
-        int box = n;
+        // 이차배열 만들기
+        int h = n % w > 0 ? n / w + 1 : n / w;
+        int[][] map = new int[h][w];
+        
+        int[] position = new int[2];
+        
+        
+        int number = 1;
+        for(int i = 0; i < h && number <= n; i++) {
+            if(i % 2 == 1) {
+                for(int j = 0; j < w && number <= n; j++) {
+                    map[i][j] = number;
 
-        // 세로 크기 (상자 개수에서 가로 크기를 나눈 몫 + 1)
-        int y = 0;
-        
-        if(n%w == 0) {
-            y = n/w;
-        } else {
-            y = n/w + 1;
-        }
+                    if(number == num) {
+                        position[0] = i;
+                        position[1] = j;
+                    }
+                    number++;
+                }
+            } else {
+                for(int j = w-1; j >= 0 && number <= n; j--) {
+                    map[i][j] = number;
 
-        // num가 몇층에있는지 확인
-        int flor = 0;
-        if(num < w) {
-            flor = 1;
-        } else if((num%w) == 0) {
-            flor = (num/w);
-        } else {
-            flor = (num/w) + 1;
+                    if(number == num) {
+                        position[0] = i;
+                        position[1] = j;
+                    }
+                    number++;
+                }
+            }
+
         }
         
-        // num에 해당하는 층에서 꼭대기 -1 까지는 무조건 꺼내야하는 박스 + 꺼내야하는 박스 (내 박스) 1
-        int outBox = y - flor;
         
-        // 이제 꼭대기에서 내 위치에 박스가 있는지 없는지 체크
-        // 층의 위치가 짝수면 오른쪽에서 왼쪽
-        // 현재 층의 위치가 홀수면 왼쪽에서 오른쪽
-        // 우선 내 위치 인덱스 찾기 현재 층수 * w - num
-        int index = 0;
-        if(flor == 1) {
-            index = num;
-        } else if(flor%2 == 0) {
-            index = ( (flor * w) - num + 1) ;
-        } else {
-            index = w - ((flor * w) - num);
+        for(int i = position[0]; i < h; i++) {
             
-        }
-
-        
-        // 최고층에 올라갈 박스 개수
-        int topBox = w - (w * y - box);
-        
-        if(y == 1 || flor == y) {
-            outBox += 1;
-        } else if(y%2 == 1) {
-            outBox += topBox >= index ? 1 : 0;  
-        } else {
-            outBox += index > w - topBox ? 1 : 0;
+            if(map[i][position[1]] > 0) {
+                answer++;
+            }
         }
         
-        return outBox;
+        
+        return answer;
     }
 }
