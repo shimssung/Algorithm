88befcbc889008133caf13ca61fad1513ec3603c
shimssung <shimssung@gmail.com>
2025-07-09 10:30:13 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        
+        if(number % n == 0) {
+            answer = number % m == 0 ? 1 : 0;
+        } else {
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
