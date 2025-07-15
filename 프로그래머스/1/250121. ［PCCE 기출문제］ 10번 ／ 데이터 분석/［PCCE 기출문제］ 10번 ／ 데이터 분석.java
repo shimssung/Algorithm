fa1465eabe5d2c5fe89d3d6dab144a0ed4a7aa0b
shimssung <shimssung@gmail.com>
2025-07-15 17:56:45 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.97 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,37 +1,40 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
-// 내가 사용한 Comparator는 정렬만 가능하지만 stream을 사용하면
-// 정렬, 매핑, 필터링, 집계 등 다양한 것들을 연속적으로 가능하기 때문에
-// stream을 배우는게 좋아보인다.
 class Solution {
-    // data: 데이터 [code, date, maxumum, remain]
-    // ext: 뽑아낼 데이터 기준
-    // val_ext: 데이터 기준값
-    // sort_by: 정렬 기준
-    // data에서 ext값이 val_ext보다 작은 데이터만 뽑은 후, sort_by에 해당하는 값을 기준으로 오름차순 정렬
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         
-        int index_ext = ext.equals("code") ? 0 : ext.equals("date") ? 1 : ext.equals("maximum") ?  2 : 3;
-        int index_sort = sort_by.equals("code") ? 0 : sort_by.equals("date") ? 1 : sort_by.equals("maximum") ?  2 : 3;
         
-        // 조건에 맞는 데이터를 필터링하여 리스트에 추가
-        // List는 동적으로 크기가 변경되기때문에 편함
-        List<int[]> filteredData = new ArrayList<>();
-        for (int[] row : data) {
-            if (row[index_ext] < val_ext) {
-                filteredData.add(row);
+        // ext : 기준
+        // val_ext : 기준값
+        // sort_by : 오름차순 기준값
+        
+        // 기준값 인덱스 저장
+        HashMap<String, Integer> sort = new HashMap<>();
+        sort.put("code", 0);
+        sort.put("date", 1);
+        sort.put("maximum", 2);
+        sort.put("remain", 3);
+        
+        List<int[]> result = new ArrayList<>();
+        int index = sort.get(ext);
+        for(int i = 0; i < data.length; i++) {
+            if(data[i][index] < val_ext) {
+                result.add(data[i]);
             }
+            
         }
         
-        // 정렬
-        filteredData.sort(Comparator.comparingInt(row -> row[index_sort]));
+        Collections.sort(result, (a, b) -> Integer.compare(a[sort.get(sort_by)], b[sort.get(sort_by)]));
+        int[][] answer = new int[result.size()][data[0].length];
+        for(int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        
+        // int[][] answer = result.toArray(new int[result.size()][]); 로도 가능
 
-        // 리스트를 배열로 변환
-        int[][] answer = new int[filteredData.size()][4];
-        for (int i = 0; i < filteredData.size(); i++) {
-            answer[i] = filteredData.get(i);
-        }
-        
         return answer;
     }
 }
