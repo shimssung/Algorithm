cf9637abf2eecae1e781c7217deea64ef7d0d4bc
shimssung <shimssung@gmail.com>
2025-07-18 11:43:46 +0900

    [level 1] Title: 추억 점수, Time: 1.40 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,30 +1,25 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
-    // 사진별 추억점수 매기기
-    // name: 그리워하는 사람의 이름을 담은 문자열 배열
-    // yearning: 각 사람별 그리움 점수를 담은 정수 배열
-    // photo: 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
         
-        // hashMap함수를 이용해 풀면될거같음.
-        HashMap<String, Integer> member = new HashMap<String, Integer>();
-        
+        // 이름별 점수 저장
+        HashMap<String, Integer> point = new HashMap<>();
         for(int i = 0; i < name.length; i++) {
-            member.put(name[i], yearning[i]);
+            point.put(name[i], yearning[i]);
         }
-        int[] answer = new int [photo.length];
         
+        // photo에 해당 이름이 언급된 횟수 저장
         for(int i = 0; i < photo.length; i++) {
+            int sum = 0;
             for(int j = 0; j < photo[i].length; j++) {
-                
-                // 점수가있을때만 추가
-                if(member.containsKey(photo[i][j])){
-                    answer[i] += member.get(photo[i][j]);
+                if(point.containsKey(photo[i][j])) {
+                    sum += point.get(photo[i][j]);
                 }
             }
+            answer[i] = sum;
         }
-        
         
         return answer;
     }
