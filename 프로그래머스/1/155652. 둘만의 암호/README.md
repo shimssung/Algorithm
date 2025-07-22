86f1f5da1363ffe14e86167cacf787413da42282
shimssung <shimssung@gmail.com>
2025-07-22 22:11:39 +0900

    [level 1] Title: 둘만의 암호, Time: 13.54 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 89.8 MB, 시간: 0.54 ms
+메모리: 84.6 MB, 시간: 13.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 00:17:00
+2025년 07월 22일 22:11:35
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,45 +1,35 @@
+import java.util.HashSet;
 
-// 문자열 s를 바꿀건데 하나씩 index만큼 더한 문자로 바꿔주려한다.
-// 단 index만큼 더해줄때 skip에 포함되어있으면 패스를 하고 넘어간다.
-// z를 넘어갈 경우 다시 a로 돌아간다.
-// a = 97, z = 122
-
-// 우선 s를 char배열로 만들고, skip은 hashMap에 넣어놓는게 좋아보임.
-// for each로 반복문을 만들어서 각 char에 들어있는 값을 +1하고 skip에 들어있는 값중 일치하는게 있다면 count를 ++하지 않는다.
-// count의 값이 index와 일치한다면 break.
-
-import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
-        // skip 문자열을 Set으로 저장 (빠른 lookup)
-        Set<Character> skipSet = new HashSet<>();
-        for (char c : skip.toCharArray()) {
-            skipSet.add(c);
-        }
+        String answer = "";
 
-        StringBuilder result = new StringBuilder();
+        // skip 문자열을 HashSet에 저장
+        HashSet<Character> c = new HashSet<>();
+        for (char k : skip.toCharArray()) {
+            c.add(k);
+        }
 
         for (char ch : s.toCharArray()) {
             int count = 0;
-            char cur = ch;
+            char nextChar = ch;
 
+            // index만큼 건너뛰며 문자 변경
             while (count < index) {
-                cur++;
+                nextChar++;
 
-                // 'z' 넘어가면 'a'로 돌아가기
-                if (cur > 'z') {
-                    cur = 'a';
+                if (nextChar > 'z') {
+                    nextChar = 'a';  // z를 넘어가면 a로
                 }
 
-                // skip에 포함되지 않으면 count++
-                if (!skipSet.contains(cur)) {
+                if (!c.contains(nextChar)) {
                     count++;
                 }
             }
 
-            result.append(cur);
+            answer += nextChar;
         }
 
-        return result.toString();
+        return answer;
     }
 }
