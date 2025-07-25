881e7d1a51937f742eb91e290167efd46b4a617d
shimssung <shimssung@gmail.com>
2025-07-25 20:32:04 +0900

    [level 1] Title: 명예의 전당 (1), Time: 11.07 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
 
 ### 성능 요약
 
-메모리: 80.8 MB, 시간: 6.38 ms
+메모리: 85.3 MB, 시간: 11.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 14일 19:26:45
+2025년 07월 25일 20:32:00
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,25 +1,23 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         
-        List<Integer> topScore = new ArrayList<>();
-        
-        for(int i = 0; i<score.length; i++) {
-            topScore.add(score[i]); // 일단 추가
-            // 정렬(오름차순)
-            Collections.sort(topScore);
-            
-            if (topScore.size() > k) {
-                topScore.remove(topScore.get(0)); // k개 초과면 가장 낮은 점수 제거
+        // 점수 넣어서 정렬하고 상위 3개만 꺼내기
+        List<Integer> s = new ArrayList<>();
+        for(int i = 0; i < score.length; i++) {
+            s.add(score[i]);
+            Collections.sort(s);
+            int n = s.size();
+            if( n > k) {
+                answer[i] = s.get(n-k);
+            } else if( n <= k) {
+                answer[i] = s.get(0);
             }
-            
-            answer[i] = topScore.get(0); // 가장 낮은 점수 저장
         }
-        
         
         return answer;
     }
