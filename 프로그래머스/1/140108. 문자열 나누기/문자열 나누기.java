898b75006ca18b392ebbc103d4e7f1515c75281f
shimssung <shimssung@gmail.com>
2025-07-25 10:53:49 +0900

    [level 1] Title: 문자열 나누기, Time: 9.49 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,24 +1,28 @@
 class Solution {
     public int solution(String s) {
+        int answer = 0;
+        int count = 0;
         
-        int answer = 1;
-        
-        char x = s.charAt(0);
-        int count = 1;
-        for(int i = 1; i < s.length(); i++) {
-            
+        String[] S = s.split("");
+        String first = S[0];
+        for(int i = 0; i < S.length; i++) {
+            if(S[i].equals(first)) {
+                count++;
+            } else if(!S[i].equals(first)) {
+                count--;
+            } 
             if(count == 0) {
-                x = s.charAt(i);
+                answer++;
+                count = 0;
+                if(i < S.length - 1) {
+                    first = S[i+1];
+                }
+            } else if(i == S.length - 1) {
                 answer++;
             }
-            
-            if(x == s.charAt(i)) {
-                count++;
-            } else {
-                count--;
-            }
         }
-
+        
+        
         return answer;
     }
 }
