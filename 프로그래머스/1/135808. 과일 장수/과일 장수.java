eb9496200fa0efabcdf3bb4bd481fce87150f022
shimssung <shimssung@gmail.com>
2025-07-28 17:19:32 +0900

    [level 1] Title: 과일 장수, Time: 45.83 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,17 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        
         int answer = 0;
         
-        // 점수 오름차순 정렬
+        // m : 한 상자에 들어가는 사과의 수
         Arrays.sort(score);
-
-        int length = score.length;
-
-        for (int i = 0; i + m <= length; i += m) {
-            int minInBox = score[length - i - m]; // 상자 내 최소 점수
-            answer += minInBox * m;
+        
+        int index = score.length;
+        // 뒤에서부터 k개수만큼 자르고 크기가 부족하면 인덱스 0반환.
+        while(index - m >= 0) {
+            index -= m;
+            answer += score[index] * m;
         }
         
         return answer;
