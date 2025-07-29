abc5e4d8b5021f22ccaa358776021ce79c2d3ff7
shimssung <shimssung@gmail.com>
2025-07-29 09:42:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 42.70 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,29 +1,22 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
-    
-    // 1 : 빵, 2: 야채, 3: 고기
-    
     public int solution(int[] ingredient) {
-        
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < ingredient.length; i++) {
-            list.add(ingredient[i]);
-            int size = list.size();
-            
-            if(size >= 4 
-               && list.get(size-4) == 1 
-               && list.get(size-3) == 2 
-               && list.get(size-2) == 3 
-               && list.get(size-1) == 1 ){
-                answer++;
-                   
-                   for(int j = 0; j < 4; j++) {
-                       list.remove(list.size() - 1);
-                   }
+        List<Integer> list = new ArrayList<>();
+        for(int i : ingredient) {
+            list.add(i);
+            int n = list.size();
+            if(n >= 4) {
+                if(list.get(n-1) == 1 && list.get(n-2) == 3 && list.get(n-3) == 2 && list.get(n-4) == 1) {
+                    answer += 1;
+                    list.remove(n-1);
+                    list.remove(n-2);
+                    list.remove(n-3);
+                    list.remove(n-4);
+                }
             }
         }
         
