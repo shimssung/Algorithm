af5000a4afee7de905da4c393769ad20fb79491b
shimssung <shimssung@gmail.com>
2025-07-31 17:30:46 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.04 ms
+메모리: 85.3 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 14:48:03
+2025년 07월 31일 17:30:44
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,14 +1,11 @@
 class Solution {
-    
-    // 3명의 학생의 정수를 더했을때 0이되면 삼총사
-    
     public int solution(int[] number) {
         int answer = 0;
         
-        for(int x = 0; x < number.length; x++) {
-            for(int y = x+1; y < number.length; y++) {
-                for(int z = y+1; z < number.length; z++){
-                    if(number[x] + number[y] + number[z] ==  0) {answer++;}
+        for(int i = 0; i < number.length-2; i++) {
+            for(int j = i+1; j < number.length; j++) {
+                for(int w = j+1; w < number.length; w++) {
+                    if(number[i]+number[j]+number[w] == 0) answer++;
                 }
             }
         }
