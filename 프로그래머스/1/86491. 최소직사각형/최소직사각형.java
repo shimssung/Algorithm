a9bebdc75f91d1d41e7b650813ec88c8356b41f8
shimssung <shimssung@gmail.com>
2025-08-02 19:29:20 +0900

    [level 1] Title: 최소직사각형, Time: 2.13 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,21 +1,18 @@
 class Solution {
     public int solution(int[][] sizes) {
+        int answer = 0;
         int x = 0;
         int y = 0;
-        
+        // 번호마다 큰건 왼쪽 작은건 오른쪽에 넣기
+        // 가로길이와 세로길이의 값이 더 큰것만 넣기
         for(int i = 0; i < sizes.length; i++) {
-            for(int j = 0; j < 2; j++) {
-                if(sizes[i][0] > sizes[i][1]) {
-                    x = x < sizes[i][0] ? sizes[i][0] : x;
-                    y = y < sizes[i][1] ? sizes[i][1] : y;
-                } else {
-                    x = x < sizes[i][1] ? sizes[i][1] : x;
-                    y = y < sizes[i][0] ? sizes[i][0] : y;
-                }
-                
-            }
+            int min = Math.min(sizes[i][0], sizes[i][1]);
+            int max = Math.max(sizes[i][0], sizes[i][1]);
+            x = Math.max(x, min);
+            y = Math.max(y, max);
         }
+        answer = x * y;
         
-        return x * y;
+        return answer;
     }
 }
