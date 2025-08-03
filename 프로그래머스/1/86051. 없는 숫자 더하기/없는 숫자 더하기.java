8d27a97e719e776358e4f4bdee2e99cd1d429a30
shimssung <shimssung@gmail.com>
2025-08-03 16:54:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,9 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -1;
-        
-        int numb = 0;
+        int answer = 45;
         
         for(int i = 0; i < numbers.length; i++) {
-            numb += numbers[i];
-        }
-        
-        answer += 45 - numb;
-        
-        if(answer > -1) {
-            answer += 1;
+            answer -= numbers[i];
         }
         
         return answer;
