99a674a9c0ca6f93958d9975bd09317592770d40
shimssung <shimssung@gmail.com>
2025-08-04 09:48:46 +0900

    [level 1] Title: 신고 결과 받기, Time: 147.50 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,48 +1,45 @@
-import java.util.HashSet;
 import java.util.HashMap;
-import java.util.Set;
-import java.util.Map;
+import java.util.HashSet;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        
         int[] answer = new int[id_list.length];
         
-        Set<String> reportSet = new HashSet<>();
+        // 한사람이 여러번 신고해도 1회로 인정.
+        // k번 이상 신고 당한 사람은 정지
+        // 정지된 사람을 신고했을경우 +1 씩 리턴.
         
-        // 중복제거
+        HashMap<String, Integer> list = new HashMap<>();
+        for(int i = 0; i < id_list.length; i++) {
+            list.put(id_list[i], i);
+        }
+        
+        // 신고당한 사람을 key로 신고한 사람을 value로 HashMap생성
+        HashMap<String, HashSet<String>> reported = new HashMap<>();
+        
         for(int i = 0; i < report.length; i++) {
-            reportSet.add(report[i]);
+            String[] report_result = report[i].split(" ");
+            String reporter = report_result[0];
+            String target = report_result[1];
+            if (!reported.containsKey(target)) {
+                reported.put(target, new HashSet<>());
+            }
+            reported.get(target).add(reporter);
         }
         
-        // 신고 기록 ( 신고당한 사람, 신고한 사람들)
-        Map<String, Set<String>> reportedBy = new HashMap<>();
-        for(String r : reportSet) {
-            String[] m = r.split(" ");
-            String reporter = m[0];
-            String reported = m[1];
-            
-            // reported라는 이름이 맵에 없으면, 새로 만들어 집어넣는다.
-            reportedBy.putIfAbsent(reported, new HashSet<>());
-            reportedBy.get(reported).add(reporter);
-        }
-        
-        // 정지된 유저 확인
-        Map<String, Integer> mailCount = new HashMap<>();
-        for (String reported : reportedBy.keySet()) {
-            Set<String> reporters = reportedBy.get(reported); // 신고자 목록
-            if (reporters.size() >= k) { // 정지 조건 만족
-                for (String reporter : reporters) {
-                    // reporter가 메일 한 통 받음
-                    mailCount.put(reporter, mailCount.getOrDefault(reporter, 0) + 1);
+        for(int i = 0; i < id_list.length; i++) {
+            if(reported.containsKey(id_list[i]) && reported.get(id_list[i]).size() >= k) {
+                for (String reporter : reported.get(id_list[i])) {
+                    int index = list.get(reporter);
+                    answer[index]++;
                 }
             }
         }
         
-        // id_list 순서대로 결과 배열 채우기
-        for (int i = 0; i < id_list.length; i++) {
-            answer[i] = mailCount.getOrDefault(id_list[i], 0);
-        }
+        
+        
+        
+        
         
         
         return answer;
