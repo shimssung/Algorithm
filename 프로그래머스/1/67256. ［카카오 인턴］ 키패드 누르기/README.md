a11198f3e537f958c4fc257ecdf46b5049790d2a
shimssung <shimssung@gmail.com>
2025-08-07 16:33:13 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.06 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 94.9 MB, 시간: 9.65 ms
+메모리: 81.5 MB, 시간: 4.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 29일 22:04:02
+2025년 08월 07일 16:33:11
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,12 +1,14 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
     public String solution(int[] numbers, String hand) {
-        
         String answer = "";
         
-        // 왼손은 *에서 시작, 오른손은 #에서 시작
+        // 왼손 : * 시작
+        // 오른손 : # 시작
+        // 1, 4, 7 왼손
+        // 3, 6, 9 오른손
+        
         int[][] keypad = {
             {1, 2, 3},
             {4, 5, 6},
@@ -14,54 +16,47 @@ class Solution {
             {-1, 0, -1}
         };
         
-        Map<Integer, int[]> numberMap = new HashMap<>();
-        Map<String, int[]> handMap = new HashMap<>();
-        
-        handMap.put("left", new int[] {3, 0});
-        handMap.put("right", new int[] {3, 2});
-        
+        HashMap<Integer, int[]> number = new HashMap<>();
         for(int i = 0; i < keypad.length; i++) {
             for(int j = 0; j < keypad[0].length; j++) {
                 if(keypad[i][j] != -1) {
-                    numberMap.put(keypad[i][j], new int[]{i, j});
+                    number.put(keypad[i][j], new int[]{i, j});
                 }
             }
         }
         
+        HashMap<String, int[]> handMap = new HashMap<>();
+        handMap.put("L", new int[]{3, 0});
+        handMap.put("R", new int[]{3, 2});
+        
         for(int i : numbers) {
             if(i == 1 || i == 4 || i == 7) {
                 answer += "L";
-                int[] location = numberMap.get(i);
-                handMap.put("left", new int[] {location[0], location[1]});
-            } else if (i == 3 || i == 6 || i == 9) {
+                handMap.put("L", number.get(i));
+            } else if(i == 3 || i == 6 || i == 9) {
                 answer += "R";
-                int[] location = numberMap.get(i);
-                handMap.put("right", new int[] {location[0], location[1]});
-            } else if(i == 2 || i == 5 || i == 8 || i == 0){
-                int[] location = numberMap.get(i);
-                int[] left = handMap.get("left");
-                int leftdiffX = Math.abs(left[0] - location[0]);
-                int leftdiffY = Math.abs(left[1] - location[1]);
-                int leftdiff = leftdiffX + leftdiffY;
+                handMap.put("R", number.get(i));
+            } else {
+                int Lcount = 0;
+                int Rcount = 0;
+                int x = number.get(i)[0];
+                int y = number.get(i)[1];
+                Lcount = Math.abs(handMap.get("L")[0] - x) + Math.abs(handMap.get("L")[1] - y);
+                Rcount = Math.abs(handMap.get("R")[0] - x) + Math.abs(handMap.get("R")[1] - y);
                 
-                int[] right = handMap.get("right");
-                int rightdiffX = Math.abs(right[0] - location[0]);
-                int rightdiffY = Math.abs(right[1] - location[1]);
-                int rightdiff = rightdiffX + rightdiffY;
-                
-                if(leftdiff > rightdiff) {
-                    answer += "R";
-                    handMap.put("right", new int[] {location[0], location[1]});
-                } else if(leftdiff < rightdiff){
+                if(Lcount < Rcount) {
                     answer += "L";
-                    handMap.put("left", new int[] {location[0], location[1]});
-                } else if (leftdiff == rightdiff){
-                    if(hand.equals("left")) {
-                        answer += "L";
-                        handMap.put("left", new int[] {location[0], location[1]});
-                    } else {
+                    handMap.put("L", number.get(i));
+                } else if(Lcount > Rcount) {
+                    answer += "R";
+                    handMap.put("R", number.get(i));
+                } else if(Lcount == Rcount) {
+                    if(hand.equals("right")) {
                         answer += "R";
-                        handMap.put("right", new int[] {location[0], location[1]});
+                        handMap.put("R", number.get(i));
+                    } else {
+                        answer += "L";
+                        handMap.put("L", number.get(i));
                     }
                 }
             }
