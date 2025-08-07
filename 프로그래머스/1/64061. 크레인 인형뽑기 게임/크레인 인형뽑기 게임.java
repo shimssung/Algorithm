90e60d50ceeb87d7c2b15d0fbafeaacba401abcb
shimssung <shimssung@gmail.com>
2025-08-07 17:19:37 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.11 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -11,21 +11,19 @@ class Solution {
         
         for(int i = 0; i < moves.length; i++) {
             for(int j = 0; j < board.length; j++) {
-                if(board[j][moves[i]-1] == 0) {
-                    continue;
-                } else {
-                    result.push(board[j][moves[i]-1]);
+                if(board[j][moves[i]-1] != 0) {
+                    
+                    if(!result.isEmpty() && result.peek() == board[j][moves[i]-1]) {
+                        result.pop();
+                        answer += 2;
+                    } else {
+                        result.push(board[j][moves[i]-1]);
+                    }
                     board[j][moves[i]-1] = 0;
                     break;
-                }
+                } 
             }
-            if(result.size() > 1) {
-                if(result.get(result.size()-1) == result.get(result.size()-2)) {
-                    answer += 2;
-                    result.pop();
-                    result.pop();
-                }
-            }
+            
         }
         
         return answer;
