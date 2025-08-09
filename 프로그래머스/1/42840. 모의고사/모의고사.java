6ddd2f06d6a034981bad410debbcac7c9fdfd548
shimssung <shimssung@gmail.com>
2025-08-09 19:21:48 +0900

    [level 1] Title: 모의고사, Time: 0.87 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,34 @@
-import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] m1 = {1,2,3,4,5};
-        int[] m2 = {2,1,2,3,2,4,2,5};
-        int[] m3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int[] score = new int[3];
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        int[] num1 = {1, 2, 3, 4, 5};
+        int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int[] count = new int[3];
         
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == m1[i%m1.length]) { score[0]++; }
-            if(answers[i] == m2[i%m2.length]) { score[1]++; }
-            if(answers[i] == m3[i%m3.length]) { score[2]++; }
+            if(answers[i] == num1[i%5]) count[0]++;
+            if(answers[i] == num2[i%8]) count[1]++;
+            if(answers[i] == num3[i%10]) count[2]++;
         }
         
-        int top = score[0] > score[1] ? score[0] : score[1];
-        top = top > score[2] ? top : score[2];
-        
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < score.length; i++) {
-            if(score[i] == top) {
-                ans.add(i+1);
-            }
+        int max = 0;
+        for(int i = 0; i < 3; i++) {
+           max = max < count[i] ? count[i] : max;
         }
         
-        int[] answer = new int[ans.size()];
-        for(int i = 0; i < ans.size(); i++) {
-            answer[i] = ans.get(i);
+        for(int i = 0; i < 3; i ++) {
+            if(count[i] == max) answer.add(i+1);
         }
-        return answer;
+        
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) result[i] = answer.get(i);
+        
+        return result;
     }
 }
