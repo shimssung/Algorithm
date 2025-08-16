770c627afe4310b8b1f872f11e3f32b75f365369
shimssung <shimssung@gmail.com>
2025-08-16 14:29:41 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i : arr) answer += i;
+        
+        answer = answer/arr.length;
+        
+        return answer;
+    }
+}
