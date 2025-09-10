a4ed791ccac8ac716bd44a0253c8ec01743a6f2f
shimssung <shimssung@gmail.com>
2025-09-10 14:46:03 +0900

    [level 1] Title: 폰켓몬, Time: 7.82 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,25 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
+        int result = nums.length/2;
+        // HashMap에 넣기
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        HashSet<Integer> result = new HashSet<>();
+        for(int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
         
-        for(int n : nums) result.add(n);
         
-        int max = nums.length/2;
+        // map.size()를 이용해 key개수 반환
+        int l = map.size();
+        answer = l <= result ? l : result;
+        // key의 개수 <= result 일때 key의 개수 반환
+        // key의 개수 > result 일때는 result 반환
         
-        return Math.min(result.size(), max);
+        
+        
+        return answer;
     }
 }
