702dad4e1aea0aad3d1a39d3fe10431535a2d76a
shimssung <shimssung@gmail.com>
2025-09-10 15:31:10 +0900

    [level 2] Title: 전화번호 목록, Time: 343.06 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,17 +2,21 @@ import java.util.HashSet;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         
-        HashSet<String> key = new HashSet<>();
+        HashSet<String> map = new HashSet<>();
         
-        for(String s : phone_book) key.add(s);
+        for(String s : phone_book) map.add(s);
         
         for(String s : phone_book) {
             for(int i = 1; i < s.length(); i++) {
-                if(key.contains(s.substring(0, i))) return false;
+                if(map.contains(s.substring(0, i))) {
+                    answer = false;
+                    break;
+                }
             }
         }
         
-        return true;
+        return answer;
     }
 }
