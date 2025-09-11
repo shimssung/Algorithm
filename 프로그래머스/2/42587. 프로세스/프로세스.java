59865e336265de3d9a38e58b1c9905cddadafa7c
shimssung <shimssung@gmail.com>
2025-09-11 17:55:57 +0900

    [level 2] Title: 프로세스, Time: 2.38 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        
         List<Integer> list = new ArrayList<>();
         Deque<Integer> que = new ArrayDeque<>();
         
         for(int i = 0; i < priorities.length; i++) {
-            map.put(i, priorities[i]);
             list.add(priorities[i]);
-            que.offer(i);
         }
         
         Collections.sort(list, (o1, o2) -> o2 - o1); // 내림차순
         
-        List<Integer> result = new ArrayList<>();
+        for(int i : list) que.offer(i);
+        int answer = 1;
+        
         while(!que.isEmpty()) {
-            int idx = que.poll();
-            if(map.get(idx) == list.get(0)) {
-                result.add(idx);
-                list.remove(0);
-            } else {
-                que.offer(idx);
+            for(int i = 0; i < priorities.length; i++) {
+                if(priorities[i] == que.peek()) {
+                    if(i == location) {
+                        return answer;
+                    }
+                    que.poll();
+                    answer++;
+                }
             }
         }
         
-        return result.indexOf(location)+1;
-    }
+        return answer;
+    }   
 }
