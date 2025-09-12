2db404501f8032b769bd40317bdcd75480c612d2
shimssung <shimssung@gmail.com>
2025-09-12 17:32:35 +0900

    [level 2] Title: 주식가격, Time: 41.25 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,26 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int len = prices.length;
+        int[] answer = new int[len];
         
-        for(int i = 0; i < prices.length-1; i++) {
-            int count = 0;
-            for(int j = i+1; j < prices.length; j++) {
-                if(prices[i] > prices[j]) {
-                    count++;
-                    break;
-                }
-                count++;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < len; i++) {
+            
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+                
             }
-            answer[i] = count;
+            stack.push(i);
         }
         
-        answer[prices.length-1] = 0;
+        while(!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = (len-1) - idx;
+        }
         
         return answer;
     }
